de9fd4b02e4dce507afc9b043a933444f0f518e2
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-05 10:29:04 +0600

    Device.java

--- src/Device.java
+++ src/Device.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Device {
+class Device {
+    private String type;
+    private double price;
+    private double weight;
+
+    public Device(String type, double price, double weight) {
+        this.type = type;
+        this.price = price;
+        this.weight = weight;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
+
