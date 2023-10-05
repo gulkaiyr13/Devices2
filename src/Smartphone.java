143a91c900d4a0a2171022f9087467e5c9afd19c
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-05 10:30:46 +0600

    Smartphone.java

--- src/Smartphone.java
+++ src/Smartphone.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Smartphone {
+class Smartphone extends Device {
+    private double screenSize;
+    private int cameraResolution;
+
+    public Smartphone(String type, double price, double weight, double screenSize, int cameraResolution) {
+        super(type, price, weight);
+        this.screenSize = screenSize;
+        this.cameraResolution = cameraResolution;
+    }
 }
+
