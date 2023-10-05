0981cea52a62de0d2c16d1e987978e3b581637fc
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-05 10:31:13 +0600

    Tablet.java

--- src/Tablet.java
+++ src/Tablet.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Tablet {
+class Tablet extends Device {
+    private String operatingSystem;
+    private boolean hasStylus;
+
+    public Tablet(String type, double price, double weight, String operatingSystem, boolean hasStylus) {
+        super(type, price, weight);
+        this.operatingSystem = operatingSystem;
+        this.hasStylus = hasStylus;
+    }
 }
