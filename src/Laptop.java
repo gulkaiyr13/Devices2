f22f301134fb9932f99374f2c49c1706cc42f524
gulkaiyr13 <145272966+gulkaiyr13@users.noreply.github.com>
2023-10-05 10:29:50 +0600

    Laptop.java

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Laptop {
+class Laptop extends Device {
+    private String processor;
+    private int ram;
+
+    public Laptop(String type, double price, double weight, String processor, int ram) {
+        super(type, price, weight);
+        this.processor = processor;
+        this.ram = ram;
+    }
 }
+
